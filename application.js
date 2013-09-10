f99e0fc3a697c365b556d98a61957133ec161f6d
whogben55 <william@getitllc.com>
2013-09-09 23:34:53 -0400

    fixes bugs

--- application.js
+++ application.js
@@ -1,28 +1,29 @@
+//make cursor start in input box
 window.onload=function() {
 document.forms[0][0].focus();
 }
-var init = 0;
+
 $(document).ready( function() {
-    $('#item').on('keydown',function(event){
+    $('#item').on('keydown',function(event){ // adds item on enter press
         if(event.keyCode == 13) {
             event.preventDefault();
             var item = $(this).val();
-            if(isBlank(item)){
+            if(isBlank(item)){ // if all blanks make text red
                 $(':input').val('Invalid Entry');
                 $(this).css('color','red');
                 return;
             }
             $(this).css('color','black');
             $(':input').val('');
-            $(this).parent().parent().parent().parent().append(listString(item));
+            $(this).parent().parent().parent().parent().append(listString(item)); //add to end of list
             $('li').last().children().first().addClass('checkbox');
         };
     });
-    $(document.body).on('click', '.checkbox', function(){
+    $(document.body).on('click', '.checkbox', function(){ // on checkbox click
         if(!$(this).next().hasClass('form') ){
             if( !$(this).parent().hasClass('checked') ){
                 $(this).parent().addClass('checked');
-                $(this).first().html('<img src="check-mark-hi.png" />');
+                $(this).first().html('<img src="check-mark-hi.png" alt="check"/>');
             }
             else{
                 $(this).parent().removeClass('checked');
@@ -37,13 +38,14 @@ $(document).ready( function() {
 });
 
 
-
-function listString(item){
+// create the list item
+function listString(item){ 
     var before = '<li><div></div><div class="set">';
-    var after = '</div></li><hr size="2" color="black">';
+    var after = '</div></li><hr>';
     return before + item + after;
 }
 
+// check if string is blank
 function isBlank(str) {
     return (!str || /^\s*$/.test(str));
 }
